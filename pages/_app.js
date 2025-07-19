624f208ee7d80eb86eaaba85e494bb6e9d576abb
rbeyzas <rukiyebeyzasarikaya@hotmail.com>
2025-07-19 22:41:30 +0300

    Implement session management in main navigation and app layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
+import { SessionProvider } from 'next-auth/react';
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <SessionProvider session={session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
   );
 }
-
-export default MyApp;
